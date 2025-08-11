1c2f377cebcb7a7fc1f13aa6b62106ab6f4139cf
Junaiyo <bandidoblackpheonix@gmail.com>
2025-08-10 22:21:42 -0300

    Details

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import "./styles/style.css";
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import {HashRouter, Routes, Route, Navigate} from "react-router-dom";
 import {App} from "./app";
 import {Admin} from "./admin/admin";
 
 const Index = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />}/>
+        <Route path="/" element={<Navigate to="/home" replace />}/>
         <Route path="/home" element={<App />}/>
         <Route path="/admin" element={<Admin />}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
